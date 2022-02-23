e8731b7409716a53a9cad6affde8d0cc47b2487e
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2022-02-23 11:29:15 +0100

    [~] Coding style on list/append_node.c

--- src/list/append_node.c
+++ src/list/append_node.c
@@ -8,36 +8,6 @@
 #include "list.h"
 #include <stdlib.h>
 
-list_t *get_from_index(list_t **begin, size_t index, size_t len_list)
-{
-    list_t *tmp = *begin;
-
-    if (index < len_list / 2) {
-        if (index % 2 == 0) {
-            for (size_t i = 0; i != index; i += 2)
-                tmp = tmp->next->next;
-        } else if (index % 3 == 0) {
-            for (size_t i = 0; i != index; i += 3)
-                tmp = tmp->next->next->next;
-        } else {
-            for (size_t i = 0; i != index; i++)
-                tmp = tmp->next;
-        }
-    } else {
-        if ((len_list - index) % 2 == 0) {
-            for (size_t i = len_list; i != index; i -= 2)
-                tmp = tmp->prev->prev;
-        } else if ((len_list - index) % 3 == 0) {
-            for (size_t i = len_list; i != index; i -= 3)
-                tmp = tmp->prev->prev->prev;
-        } else {
-            for (size_t i = len_list; i != index; i--)
-                tmp = tmp->prev;
-        }
-    }
-    return tmp;
-}
-
 void append_node(list_t **begin, void *data)
 {
     list_t *new = malloc(sizeof(list_t));
